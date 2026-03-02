47a3288c2f2a8aa9621ece8002d49b9ae2e8c211
devendramali12 <dmali9198@gmail.com>
2026-03-02 09:01:05 +0530

    init commit

--- first-project/src/css-react/Councellor.jsx
+++ first-project/src/css-react/Councellor.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import index from "./councellor.module.css";
 const Councellor = () => {
+  console.log(index);
   return (
     <>
-      <h2>Councellor</h2>
-      <p>
+      <h2 className={index.heading}>Councellor : Sudhir</h2>
+      <p className={index.para}>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
         eaque?
       </p>
